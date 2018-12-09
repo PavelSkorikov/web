82c353371143547b52a38f217202da317ec7c650
PavelSkorikov <spg75@yandex.ru>
2018-12-09 16:24:46 +0300

    11

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
+from cgi import parse_qs
 def app(environ, start_response):
-        data = environ['QUERY_STRING'].split('&')
+        """data = [bytes(i+'\n', encoding = 'ascii') for i in environ['QUERY_STRING'].split('&')]
         start_response("200 OK", [
             ("Content-Type", "text/plain"),
             ("Content-Length", str(len(data)))
-        ])
-        for i in data:
-            yield i
+        ])"""
+        start_response('200 OK', [('Content-Type', 'text/plain')])
+        qs = parse_qs(environ['QUERY_STRING'])
+        return  ['%s=%s<br>' % (k, qs[k][0]) for k in qs]
