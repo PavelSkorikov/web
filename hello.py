cb0ac82f413ca70d728ca6cd8ee89c017e020296
PavelSkorikov <spg75@yandex.ru>
2018-12-09 17:49:21 +0300

    encode('utf-8')

--- hello.py
+++ hello.py
@@ -3,5 +3,5 @@ def app (environ, start_response):
   response_headers = [('Content-type','text/plain')]
   start_response(status, response_headers)
   resp = environ['QUERY_STRING'].split("&")
-  resp = [item+"\r\n" for item in resp]
+  resp = [(item+"\r\n").encode('utf-8') for item in resp]
   return resp
