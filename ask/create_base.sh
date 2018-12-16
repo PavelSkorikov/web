1c2f293e31bb184f812e688bc01f707c72e1e5b5
PavelSkorikov <spg75@yandex.ru>
2018-12-16 14:44:03 +0300

    mysql1

--- ask/create_base.sh
+++ ask/create_base.sh
@@ -1,4 +1,4 @@
 mysql -uroot -e "CREATE DATABASE qabase"
 mysql -uroot -e "CREATE USER 'pavel'@'localhost' IDENTIFIED BY '123456'"
 mysql -uroot -e "GRANT ALL ON qabase.* TO 'pavel'@'localhost'"
-manage.py syncdb
+python3 manage.py syncdb
--- ask/qa/models.py
+++ ask/qa/models.py
@@ -15,7 +15,7 @@ class Question(models.Model):
     added_at = models.DateTimeField(blank=True, auto_now_add=True)
     rating = models.IntegerField(default = 0)
     author = models.ForeignKey(User, null=False, on_delete=models.PROTECT)
-    likes = models.ManyToMany.Field(User, related_name='likes_set')
+    likes = models.ManyToManyField(User, related_name='likes_set')
 
 class Answer(models.Model):
     text = models.TextField()
