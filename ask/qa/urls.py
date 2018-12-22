c65693a5168cc88ded97c3811a685aa72424aca6
PavelSkorikov <spg75@yandex.ru>
2018-12-22 21:22:48 +0300

    question4

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import url
 from django.http import Http404
 from django.shortcuts import render
 
-from . import views
+from qa.views import new_question_list, popular_question_list, question
 
 urlpatterns = [
-    url(r'^', qa.views.new_question_list, name='new_question_list'),
-    url(r'^popular/', qa.views.popular_question_list, name='popular_question_list'),
-    url(r'^question/(?P<slug>\d+)/$', qa.views.question, name='question'),
+    url(r'^', new_question_list, name='new_question_list'),
+    url(r'^popular/', popular_question_list, name='popular_question_list'),
+    url(r'^question/(?P<slug>\d+)/$', question, name='question'),
 ]
