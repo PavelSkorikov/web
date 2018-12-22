0adcfebd4309de34521eb3bf10e2454d3e62b201
PavelSkorikov <spg75@yandex.ru>
2018-12-22 21:15:06 +0300

    question3

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from . import views
 
 urlpatterns = [
-    url(r'^', new_question_list, name='new_question_list'),
-    url(r'^popular/', popular_question_list, name='popular_question_list'),
-    url(r'^question/(?P<slug>\d+)/$', question, name='question'),
+    url(r'^', qa.views.new_question_list, name='new_question_list'),
+    url(r'^popular/', qa.views.popular_question_list, name='popular_question_list'),
+    url(r'^question/(?P<slug>\d+)/$', qa.views.question, name='question'),
 ]
