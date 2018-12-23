82134a08f38a1a8d222305fddba45916982ff7e3
PavelSkorikov <spg75@yandex.ru>
2018-12-23 10:20:50 +0300

    question8

--- init.sh
+++ init.sh
@@ -2,6 +2,6 @@ sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 cd /home/box/web/ask/
 #sudo gunicorn -b 0.0.0.0:8000 ask.wsgi:application
-python /home/box/web/ask/manage.py runserver 0.0.0.0:8000
+sudo python /home/box/web/ask/manage.py runserver 0.0.0.0:8000
 sudo /etc/init.d/mysql start
 
