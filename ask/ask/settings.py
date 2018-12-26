94aa2b458e0db5b35f618fc708a1d3d5d7b44ab1
PavelSkorikov <spg75@yandex.ru>
2018-12-26 18:57:49 +0300

    question16

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'ask.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['../qa/templates'],
+        'DIRS': [BASE_DIR + '/qa/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,4 +121,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = BASE_DIR + '/qa/static/'
--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -7,5 +7,5 @@ from qa.views import new_question_list, popular_question_list, question
 urlpatterns = [
     url(r'^\?page=2', new_question_list, name='new_question_list'),
     url(r'^popular/\?page=3', popular_question_list, name='popular_question_list'),
-    url(r'^question/(?P<pk>\d+)/$', question, name='question'),
+    url(r'^question/(?P<pk>\d+)/', question, name='question'),
 ]
