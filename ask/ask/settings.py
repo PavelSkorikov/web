d129f75391999739bcc1a2e8f79d11672227d6c1
PavelSkorikov <spg75@yandex.ru>
2018-12-27 21:01:21 +0300

    question22

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -79,7 +79,12 @@ DATABASES = {
         'NAME': 'qabase',
         'ENGINE': 'django.db.backends.mysql',
         'USER': 'pavel',
-        'PASSWORD': '123456'
+        'PASSWORD': '123456',
+        'SERVER': 'localhost',
+        'PORT': 3306,
+        'OPTIONS': {
+              'init_command'  : "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1", 'charset': 'utf8mb4'
+                }
     }
 
 }
