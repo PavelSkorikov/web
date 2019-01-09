915ce425ac0b24d3eb99fa94c5ba726b2307a5cf
PavelSkorikov <spg75@yandex.ru>
2019-01-09 22:10:39 +0300

    users3

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'popular', popular_question_list, name='popular_question_list'),
     url(r'question/(?P<pk>\d+)/', answer_add, name='answer_add'),
     url(r'ask', question_add, name='question_add'),
-    url(r'signup/', signup, name='signup'),
-    url(r'login/', login_view, name='login_view'),
-    url(r'logout/', logout_view, name='logout_view'),
+    url(r'signup/$', signup, name='signup'),
+    url(r'login/$', login_view, name='login_view'),
+    url(r'logout/$', logout_view, name='logout_view'),
 ]
