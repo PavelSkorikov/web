04c4c17ee621e4e315468d112af64ba2933aad25
PavelSkorikov <spg75@yandex.ru>
2019-01-09 22:00:03 +0300

    users2

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -6,7 +6,7 @@ from qa.views import popular_question_list, answer_add, question_add, signup, lo
 urlpatterns = [
     url(r'popular', popular_question_list, name='popular_question_list'),
     url(r'question/(?P<pk>\d+)/', answer_add, name='answer_add'),
-    url(r'ask/', question_add, name='question_add'),
+    url(r'ask', question_add, name='question_add'),
     url(r'signup/', signup, name='signup'),
     url(r'login/', login_view, name='login_view'),
     url(r'logout/', logout_view, name='logout_view'),
